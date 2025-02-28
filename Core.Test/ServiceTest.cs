59624acf046e77ed2957515445fd0592a49e4550
Starinin Andrey <starinin-andrey@ya.ru>
2025-02-28 21:00:31 +0300

    Update

--- Core.Test/ServiceTest.cs
+++ Core.Test/ServiceTest.cs
@@ -46,4 +46,36 @@ public class ServiceTest
         
         Assert.Equal(expected, actual);
     }
+
+    [Fact]
+    public void AddPerson_PositiveTest()
+    {
+        var expected = new List<Person>()
+        {
+            new()
+            {
+                Id = new Guid("8d2c66dc-37ee-478a-9642-cd88ff250e44"),
+                LastName = "Starinin",
+                FirstName = "Andrey"
+            },
+            new()
+            {
+                Id = new Guid("fbfd87bd-0a98-4b5c-b81c-7a573f641307"),
+                LastName = "Ivanov",
+                FirstName = "Ivan"
+            },
+            new()
+            {
+                Id = new Guid("8d2c66dc-37ee-478a-9642-cd88ff250e44"),
+                LastName = "Petrov",
+                FirstName = "Petr"
+            }
+        };
+        
+        var service = new Service("test_appsettings.json");
+        service.AddPerson(new Person() { LastName = "Petrov", FirstName = "Petr" });
+        var actual = service.GetAllPersons();
+        
+        Assert.Equal(expected, actual);
+    }
 }
